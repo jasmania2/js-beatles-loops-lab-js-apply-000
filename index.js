28442757e7f39ca25111e003462f7c4f2c4835fb
sensible-draft-9261 <jasmine.williamson@me.com>
2020-02-21 18:12:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay(musicians,instruments){
   }return array;
 }
 
-function johnLennonFacts(facts){
+function johnLennonFacts(){
   let array=[];
   let i = 0;
   while (i<facts.length){
