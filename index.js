4674a223cd4edf3caaa79454246e751c14861147
sensible-draft-9261 <jasmine.williamson@me.com>
2020-02-21 17:09:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  let array = [];
+  for(let m = 0; m <= 3; m++){
+    array.push(musicians[m] + " plays " + instruments[m]);
+  }return array;
+}
+
+function johnLennonFacts(facts){
+  
+}
+
+function iLoveTheBeatles(n){
+
+}
